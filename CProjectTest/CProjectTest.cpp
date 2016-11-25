e644a0b43c4bd5942feca529f004bf98af5b046d
tomclock <wangjij@gmail.com>
2016-11-25 22:38:30 +0900

    This is just start

--- CProjectTest/CProjectTest.cpp
+++ CProjectTest/CProjectTest.cpp
@@ -1,11 +1,9 @@
 // CProjectTest.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
-
-
 int main()
 {
+	printf("This is the first C Programming test ");
     return 0;
 }
 
